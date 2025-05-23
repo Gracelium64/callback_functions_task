f5502609a3f30af2760cde43b34f727e72d55f67
Grace <grace6424@icloud.com>
2025-05-23 12:21:52 +0200

    AppBar

--- lib/src/features/main_screen/presentation/main_screen.dart
+++ lib/src/features/main_screen/presentation/main_screen.dart
@@ -17,53 +17,58 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     int finalCounter = cardCounter1 + cardCounter2 + cardCounter3;
 
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            spacing: 16,
-            children: [
-              InputCounterBox(
-                cardColor: Colors.lime,
-                onChanged: () {
-                  debugPrint('Box 1 changed');
-                },
-                onType: (int count) {
-                  setState(() {
-                    cardCounter1 = count;
-                  });
-                },
-              ),
-              InputCounterBox(
-                cardColor: Colors.cyan,
-                onChanged: () {
-                  debugPrint('Box 2 changed');
-                },
-                onType: (int count) {
-                  setState(() {
-                    cardCounter2 = count;
-                  });
-                },
-              ),
-              InputCounterBox(
-                cardColor: Colors.purpleAccent,
-                onChanged: () {
-                  debugPrint('Box 3 changed');
-                },
-                onType: (int count) {
-                  setState(() {
-                    cardCounter3 = count;
-                  });
-                },
-              ),
-              Text(
-                finalCounter < 250
-                    ? 'Total characters = $finalCounter'
-                    : 'STAHP!!1',
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 112, vertical: 50),
+          child: Text('5.1.2'),
+        ),
+        title: Text('Character Counter'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          spacing: 16,
+          children: [
+            InputCounterBox(
+              cardColor: Colors.lime,
+              onChanged: () {
+                debugPrint('Box 1 changed');
+              },
+              onType: (int count) {
+                setState(() {
+                  cardCounter1 = count;
+                });
+              },
+            ),
+            InputCounterBox(
+              cardColor: Colors.cyan,
+              onChanged: () {
+                debugPrint('Box 2 changed');
+              },
+              onType: (int count) {
+                setState(() {
+                  cardCounter2 = count;
+                });
+              },
+            ),
+            InputCounterBox(
+              cardColor: Colors.purpleAccent,
+              onChanged: () {
+                debugPrint('Box 3 changed');
+              },
+              onType: (int count) {
+                setState(() {
+                  cardCounter3 = count;
+                });
+              },
+            ),
+            Text(
+              finalCounter < 250
+                  ? 'Total characters = $finalCounter'
+                  : 'STAHP!!1',
+            ),
+          ],
         ),
       ),
     );
